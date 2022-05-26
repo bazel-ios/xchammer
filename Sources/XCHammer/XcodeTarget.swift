c4df8af7fdc3627af5558da0e447544fa41cd866
Thiago Cruz <thiago.hmcruz@gmail.com>
2022-05-26 11:23:39 -0400

    [rules_ios] Collect .a and sdk_frameworks (#24)
    * Fix missing .a deps
    * Fix missing sdk_fmwks

--- Sources/XCHammer/XcodeTarget.swift
+++ Sources/XCHammer/XcodeTarget.swift
@@ -1255,7 +1255,7 @@ public class XcodeTarget: Hashable, Equatable {
     }()
 
     func extractLibraryDeps(map targetMap: XcodeTargetMap) -> [String] {
-        return transitiveTargets(map: targetMap, predicate: stopAfterNeedsRecursive)
+        return transitiveTargets(map: targetMap, predicate: keepGoing)
             .filter { $0.type == "objc_import" }
             .compactMap { target -> [String]? in
                 guard let archives = target.attributes[.archives] as? [[String: Any]] else {
@@ -1268,7 +1268,7 @@ public class XcodeTarget: Hashable, Equatable {
 
     func extractAttributeArray(attr: RuleEntry.Attribute, map targetMap: XcodeTargetMap) -> [String] {
         if needsRecursiveExtraction {
-            return ([self] + transitiveTargets(map: targetMap, predicate: stopAfterNeedsRecursive))
+            return ([self] + transitiveTargets(map: targetMap, predicate: keepGoing))
                 .flatMap { $0.attributes[attr] as? [String] ?? [] }
         } else {
             return attributes[attr] as? [String] ?? []
